25f867df0eeca8eb4217886b3af1827fa885fb88
Marshall Adams <rma.kona@gmail.com>
2012-02-27 15:28:43 -0800

    added phpliteadmin access

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,17 +1,22 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-	protected function _initAutoload()
-	{
-		$moduleLoader = new Zend_Application_Module_Autoloader(array(
-			'namespace' => '',
-			'basePath' => APPLICATION_PATH));
-		
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+
+    protected function _initAutoload() {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+                    'namespace' => '',
+                    'basePath' => APPLICATION_PATH));
+
         //$autoloader = Zend_Loader_Autoloader::getInstance();
-        //$autoloader->setFallbackAutoloader(true);	
-		
-		return $moduleLoader;
-	}
+        //$autoloader->setFallbackAutoloader(true);
+
+        return $moduleLoader;
+    }
+
+//    protected function _initRoutes() {
+//        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/routes.ini', 'production');
+//        $router = Zend_Controller_Front::getInstance()->getRouter();
+//        $router->addConfig($config, 'routes');
+//    }
 
 }
